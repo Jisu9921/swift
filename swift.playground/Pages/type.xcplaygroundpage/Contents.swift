c194202ed066a2362da67370052dcaff5379c13e
CHOI jisu <h41469921@gmail.com>
2017-11-26 10:36:36 +0900

    Type 두 번째 예제

---
+++ swift.playground/Pages/type.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+
+let townName: String = "Knowhere"
+var unemployment: Int = 1000
+let townDescription: String = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights and \(unemployment) unemployment."
+
+print(townDescription)
